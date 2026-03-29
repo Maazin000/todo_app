d186bd0f39ac0a9f28e2bc10a38825c31f9952ff
Adwaith-raj <adwaithraj16@gmail.com>
2026-03-29 12:34:58 +0530

    update server.js

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(express.static("public"));
 
-// Connect MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/todoApp")
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.log("❌ MongoDB Connection Error:", err));
 
-// Use routes
 app.use("/", taskRoutes);
 
-// Start server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
